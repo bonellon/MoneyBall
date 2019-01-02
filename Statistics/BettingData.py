a4e32f4d5bcff6bd2a8cc47325faecb9c85c8d4b
Nicky-PC\Nicky <nbonelloghio@hotmail.com>
2019-01-02 18:24:09 +0100

    added functions

--- Statistics/BettingData.py
+++ Statistics/BettingData.py
@@ -1,35 +1,48 @@
 import json
-
 import requests
 
 mashapeKey = "trr4b4xsHumshQ6nTWYhZnzZEdUnp1VvuQEjsnes6a8aaI5vgr"
 
+
+def getEPLleagueID():
+    response = requests.get("https://api-football-v1.p.mashape.com/leagues",
+                            headers={
+                                "X-Mashape-Key": mashapeKey,
+                                "Accept": "application/json"
+                            }
+                            )
+    test = json.loads(response.text)
+    leagues2 = test['api']['leagues']
+    league_id = 0
+
+    for i in leagues2:
+        league_name = leagues2[i]['name']
+        league_country = leagues2[i]['country']
+        league_season = leagues2[i]['season']
+        if league_name == "Premier League" and league_country == "England" and league_season == "2018":
+            league_id = i
+            break
+    return league_id
+
+
+# get list of teams in league
+def getTeams(leagueID):
+    response = requests.get("https://api-football-v1.p.mashape.com/teams/league/" + leagueID,
+                            headers={
+                                "X-Mashape-Key": mashapeKey,
+                                "Accept": "application/json"
+                            }
+                            )
+
+    data = json.loads(response.content)
+    teams = data['api']['teams']
+    return teams
+
+
 # get League data
 # English Premier League =  2
-
-response = requests.get("https://api-football-v1.p.mashape.com/leagues",
-
-                        headers={
-                            "X-Mashape-Key": mashapeKey,
-                            "Accept": "application/json"
-                        }
-                        )
-test = json.loads(response.text)
-leagues = test
-leagues2 = test['leagues']
-leagueID = 2
-
-#get list of teams in league
-response = requests.get("https://api-football-v1.p.mashape.com/teams/league/2",
-  headers={
-    "X-Mashape-Key": mashapeKey,
-    "Accept": "application/json"
-  }
-)
-
-data = json.loads(response.content)
-print(data.api.teams)
-
+leagueID = getEPLleagueID()
+teams = getTeams(leagueID)
 
 # get list of next gameweek fixtures
 
