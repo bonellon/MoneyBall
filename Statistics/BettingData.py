f662d7763b62a0bbfa984d9557d2bcd75143ad75
Nicky-PC\Nicky <nbonelloghio@hotmail.com>
2019-01-03 20:08:09 +0100

    getOdds per fixtureID

--- Statistics/BettingData.py
+++ Statistics/BettingData.py
@@ -1,53 +1,131 @@
 import json
 import requests
 
+isTest = False
+currentGameweek = 21
 mashapeKey = "trr4b4xsHumshQ6nTWYhZnzZEdUnp1VvuQEjsnes6a8aaI5vgr"
 
 
+def writeResponseToFile(fileName, text):
+    file = open(fileName, 'w')
+    file.write(text)
+    file.close
+
+
 def getEPLleagueID():
-    response = requests.get("https://api-football-v1.p.mashape.com/leagues",
-                            headers={
-                                "X-Mashape-Key": mashapeKey,
-                                "Accept": "application/json"
-                            }
-                            )
-    test = json.loads(response.text)
-    leagues2 = test['api']['leagues']
+    fileName = 'getLeagues.txt'
+    if isTest:
+        with open(fileName, 'r') as file:
+            responseText = file.read()
+
+    else:
+        response = requests.get("https://api-football-v1.p.mashape.com/leagues",
+                                headers={
+                                    "X-Mashape-Key": mashapeKey,
+                                    "Accept": "application/json"
+                                }
+                                )
+        responseText = response.text
+        writeResponseToFile(fileName, responseText)
+
+    data = json.loads(responseText)
+    leagues = data['api']['leagues']
     league_id = 0
 
-    for i in leagues2:
-        league_name = leagues2[i]['name']
-        league_country = leagues2[i]['country']
-        league_season = leagues2[i]['season']
+    for i in leagues:
+        league_name = leagues[i]['name']
+        league_country = leagues[i]['country']
+        league_season = leagues[i]['season']
         if league_name == "Premier League" and league_country == "England" and league_season == "2018":
             league_id = i
             break
+
     return league_id
 
 
 # get list of teams in league
 def getTeams(leagueID):
-    response = requests.get("https://api-football-v1.p.mashape.com/teams/league/" + leagueID,
-                            headers={
-                                "X-Mashape-Key": mashapeKey,
-                                "Accept": "application/json"
-                            }
-                            )
+    fileName = 'getTeams.txt'
+    if isTest:
+        with open(fileName, 'r') as file:
+            responseText = file.read()
 
-    data = json.loads(response.text)
+    else:
+        response = requests.get("https://api-football-v1.p.mashape.com/teams/league/" + leagueID,
+                                headers={
+                                    "X-Mashape-Key": mashapeKey,
+                                    "Accept": "application/json"
+                                }
+                                )
+        responseText = response.text
+        writeResponseToFile(fileName, responseText)
+
+    data = json.loads(responseText)
     return data['api']['teams']
 
 
+# get list of next gameweek fixtures
+def getFixtures(leagueID):
+    fileName = "getFixtures.txt"
+    if isTest:
+        with open(fileName, 'r') as file:
+            responseText = file.read()
 
-# get League data
-# English Premier League =  2
-leagueID = getEPLleagueID()
-teams = getTeams(leagueID)
+    else:
+        response = requests.get("https://api-football-v1.p.mashape.com/fixtures/league/"+ leagueID,
+                               headers={
+                                   "X-Mashape-Key": "trr4b4xsHumshQ6nTWYhZnzZEdUnp1VvuQEjsnes6a8aaI5vgr",
+                                   "Accept": "application/json"
+                               }
+                               )
+        responseText = response.text
+
+    data = json.loads(responseText)["api"]["fixtures"]
+    writeResponseToFile(fileName, responseText)
+    cleanFixtureFile(data)
+
+
+def cleanFixtureFile(data):
+    cleanedFixtures = []
+    for fixture in data:
+        current = data[fixture]['round']
+        if current == ("Premier League - " + str(currentGameweek)):
+            cleanedFixtures.append(data[fixture])
+
+    file = open("getCleanedFixtures.txt", 'w')
+    for fixture in cleanedFixtures:
+        file.write(str(fixture))
+        file.write(",")
+    file.close
+    return cleanedFixtures
+
+def getOdds(fixtureID):
+    fileName = "getOdds.txt"
+    if isTest:
+        with open(fileName, 'r') as file:
+            responseText = file.read()
+
+    else:
+        response = requests.get("https://api-football-v1.p.mashape.com/odds/"+str(fixtureID),
+                               headers={
+                                   "X-Mashape-Key": "trr4b4xsHumshQ6nTWYhZnzZEdUnp1VvuQEjsnes6a8aaI5vgr",
+                                   "Accept": "application/json"
+                               }
+                               )
+        responseText = response.text
+
+    data = json.loads(responseText)["api"]["odds"]
+    writeResponseToFile(fileName, responseText)
+
+# leagueID = getEPLleagueID()
+#getFixtures("2")
+getOdds(271)
+teams = []#getTeams("2")
 teamIDs = []
 for team in teams:
     teamIDs.append(teams[team]['team_id'])
 print(teamIDs)
-# get list of next gameweek fixtures
+
 
 # for each game in next gameweek; get odds
 
