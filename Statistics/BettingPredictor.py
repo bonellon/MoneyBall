6d6513be1af217954f61e67216cae1198ba7f4a0
Nicky <bonellon@tcd.ie>
2019-01-10 13:27:46 +0100

    added new file - move to PC

---
+++ Statistics/BettingPredictor.py
@@ -0,0 +1,17 @@
+#Goalkeepers + Defenders
+#Result and Both teams to score 1/n N/n
+def calculateBestDefense():
+    with open('odds/Result&The2TeamsScore.csv', 'r') as file:
+
+
+
+
+#Midfielders
+
+
+
+
+
+
+#Striker
+#Result and both teams to score 1/y n/y
