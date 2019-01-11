0a297e577c81e0bda38e9f63eb7d256499dd6b8c
Nicky-PC\Nicky <nbonelloghio@hotmail.com>
2019-01-11 14:43:01 +0100

    bestOffence implemented

--- Statistics/BettingPredictor.py
+++ Statistics/BettingPredictor.py
@@ -3,14 +3,14 @@ import csv
 
 # Goalkeepers + Defenders
 # Result and Both teams to score 1/n N/n
-def calculateBestDefense():
+def calculateBestDefense(keep):
 
     teamProbabilities = []
     with open('odds/Result&The2TeamsScore.csv', 'r') as file:
 
         csv_reader = csv.reader(file, delimiter=',')
         isFirst = True
-        keep = ['Team1', 'Team2', '%1 & No', '%2 & No', '%N & No']
+
         keepPosition = []
         for row in csv_reader:
             if isFirst:
@@ -30,11 +30,40 @@ def calculateBestDefense():
 
     sortedProbabilities = sorted(teamProbabilities, key=lambda x: x[1])
     return sortedProbabilities
-# Midfielders
 
 
-# Striker
+# Midfielders & Strikers
+def calculateBestOffence(keep):
+    teamProbabilities = []
+    with open('odds/Result&The2TeamsScore.csv', 'r') as file:
+
+        csv_reader = csv.reader(file, delimiter=',')
+        isFirst = True
+
+        keepPosition = []
+        for row in csv_reader:
+            if isFirst:
+                for i in range(len(row)):
+                    if row[i] in keep:
+                        keepPosition.append(i)
+            else:
+                fixture = []
+                for position in keepPosition:
+                    fixture.append(row[position])
+
+                team1 = [fixture[0], round(float(fixture[2]) + float(fixture[3]) + float(fixture[6]), 2)]
+                team2 = [fixture[1], round(float(fixture[4]) + float(fixture[5]) + float(fixture[6]), 2)]
+                teamProbabilities.append(team1)
+                teamProbabilities.append(team2)
+            isFirst = False
+
+    sortedProbabilities = sorted(teamProbabilities, key=lambda x: x[1])
+    return sortedProbabilities
+
 # Result and both teams to score 1/y n/y
 
+keepDefense = ['Team1', 'Team2', '%1 & No', '%2 & No', '%N & No']
+keepOffence = ['Team1', 'Team2', '%1 & No','%1 & Yes', '%2 & No', '%2 & Yes', '%N & Yes']
 
-calculateBestDefense()
+#calculateBestDefense(keepDefense)
+calculateBestOffence(keepOffence)
