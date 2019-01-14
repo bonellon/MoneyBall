d6a886d67374e85f3c368b5de1bde97103ccaa84
Nicky <bonellon@tcd.ie>
2019-01-14 17:40:17 +0000

    Position Specific Statistical CSVs

--- Statistics/fpl.py
+++ Statistics/fpl.py
@@ -2,8 +2,85 @@
 
 import csv
 import json
-
 import requests
+
+requiredColumns = ['id',
+                   'web_name',
+                   'team_code',
+                   'element_type',
+                   'status',
+                   'now_cost',
+                   'chance_of_playing_next_round',
+                   'value_season',
+                   'minutes',
+                   'goals_scored',
+                   'assists',
+                   'saves',
+                   'clean_sheets',
+                   'goals_conceded',
+                   'bps',
+                   'ict_index',
+                   'form',
+                   'ea_index',
+                   'threat',
+                   'transfers_in',
+                   'transfers_in_event',
+                   'transfers_out',
+                   'transfers_out_event',
+                   'total_points',
+                   'ep_next']
+
+def getElementType(object):
+    for element in object:
+        if element == "element_type":
+            return object[0]
+    return 0
+
+def CleanDataCSV():
+    contents = []
+    elementTypePosition = 0
+    with open(csvfile, "r", newline='') as fp:
+        reader = csv.reader(fp)
+        readCSV = list(reader)
+        rows = []
+        for i in range(len(readCSV[0])):
+            if readCSV[0][i] in requiredColumns:
+                rows.append(i)
+                if readCSV[0][i] == "element_type":
+                    elementTypePosition = i
+
+    isFirst = True
+
+    for row in readCSV:
+        rowContents = []
+        for i in rows:
+            rowContents.append(row[i])
+        if isFirst:
+            writeToCSV("stats/goalkeepers.csv", rowContents, "w")
+            writeToCSV("stats/defenders.csv", rowContents, "w")
+            writeToCSV("stats/midfielders.csv", rowContents, "w")
+            writeToCSV("stats/forwards.csv", rowContents, "w")
+            isFirst = False
+        else:
+            filename = "undefined.csv"
+            elementType = int(row[elementTypePosition])
+
+            if elementType == 1:
+                filename = "stats/goalkeepers.csv"
+            elif elementType == 2:
+                filename = "stats/defenders.csv"
+            elif elementType == 3:
+                filename = "stats/midfielders.csv"
+            elif elementType == 4:
+                filename = "stats/forwards.csv"
+            writeToCSV(filename, rowContents, "a")
+
+
+def writeToCSV(filename, object, writeOrAppend):
+    with open(filename, writeOrAppend, newline='') as fp:
+        wr = csv.writer(fp, dialect="excel")
+        wr.writerow(object)
+
 
 url = "https://fantasy.premierleague.com/drf/bootstrap-static"
 r = requests.get(url)
@@ -22,7 +99,7 @@ count = 0
 n = 1
 
 csvfile = "data.csv"
-with open(csvfile, "w") as fp:
+with open(csvfile, "w", newline='') as fp:
     wr = csv.writer(fp, dialect="excel")
     print(all_players[0])
     wr.writerow(all_players[0])
@@ -32,6 +109,8 @@ with open(csvfile, "w") as fp:
             print(v)
             rowList.append(v)
         wr.writerow(rowList)
+
+CleanDataCSV()
 '''
 try:
     for i in player_dict:
