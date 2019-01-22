227292f3400c0e2097be54bbe67647292fac7e53
Nicky <bonellon@tcd.ie>
2019-01-22 12:58:16 +0000

    removed injured (<75% chance playing) players

--- Statistics/fpl.py
+++ Statistics/fpl.py
@@ -2,6 +2,8 @@
 
 import csv
 import json
+from collections import OrderedDict
+
 import requests
 
 url = "https://fantasy.premierleague.com/drf/bootstrap-static"
@@ -11,31 +13,54 @@ requiredColumns = ['id', 'web_name', 'team_code', 'element_type', 'status', 'now
                    'bps', 'ict_index', 'form', 'ea_index', 'threat', 'transfers_in', 'transfers_in_event',
                    'transfers_out', 'transfers_out_event', 'total_points', 'ep_next', 'event_points']
 
+commonColumns = ['id', 'web_name', 'team_code', 'element_type', 'status', 'now_cost', 'chance_of_playing_next_round',
+                 'value_season', 'minutes', 'bps', 'ict_index', 'form', 'transfers_in_event',
+                 'transfers_out_event','total_points', 'ep_next', 'event_points']
+
+goalkeeperColumns = ['clean_sheets', 'goals_conceded']
+defenderColumns = ['clean_sheets', 'goals_conceded', 'threat']
+midfielderColumns = ['goals_scored', 'assists', 'clean_sheets', 'goals_conceded', 'threat']
+forwardColumns = ['goals_scored', 'assists', 'threat']
+
+def getFPLData():
+    r = requests.get(url)
+    data = json.loads(r.text)
+    all_players = data['elements']
+    return all_players
+
 def getElementType(object):
     for element in object:
         if element == "element_type":
             return object[0]
     return 0
 
-def CleanDataCSV():
+def CleanDataCSV(players):
     elementTypePosition = 0
-    with open(csvfile, "r", newline='') as fp:
-        reader = csv.reader(fp)
-        readCSV = list(reader)
 
-        #Define empty list with size requiredColumns
-        rows = [None] * len(requiredColumns)
+    #Define empty list with size requiredColumns
+    rows = [None] * len(requiredColumns)
 
-        #Populate rows
-        for i in range(len(readCSV[0])):
-            if readCSV[0][i] in requiredColumns:
-                rows[requiredColumns.index(readCSV[0][i])] = i
+    #Populate rows
+    newPlayers = {}
+    for player in players:
+        newplayer = {}
+        if player['element_type'] == 1:
+            newplayer = getPlayerData(player, goalkeeperColumns)
+        elif player['element_type'] == 2:
+            newplayer = getPlayerData(player, defenderColumns)
+        elif player['element_type'] == 3:
+            newplayer = getPlayerData(player, midfielderColumns)
+        elif player['element_type'] == 4:
+            newplayer = getPlayerData(player, forwardColumns)
 
-                if readCSV[0][i] == "element_type":
-                    elementTypePosition = i
+        newPlayers[newplayer["id"]] = newplayer
 
+    print(len(newPlayers))
+    newPlayers = checkPlayerStatus(newPlayers)
+    print(len(newPlayers))
+    '''
     isFirst = True
-
+    
     for row in readCSV:
         rowContents = []
         for i in rows:
@@ -59,6 +84,31 @@ def CleanDataCSV():
             elif elementType == 4:
                 filename = "stats/forwards.csv"
             writeToCSV(filename, rowContents, "a")
+    '''
+
+#Iterate through players dict and return all that have chance of playing next round higher than 75%
+def checkPlayerStatus(players):
+
+    newDict = {}
+    for key, value in players.items():
+        if (value['chance_of_playing_next_round'] is not None) and (int(value['chance_of_playing_next_round']) > 75):
+            newDict[key] = value
+
+    return newDict
+
+
+def getPlayerData(player, columns):
+    list = commonColumns + columns
+    newlist = []
+    for item in list:
+        newlist.append([item, ''])
+
+    newPlayer = OrderedDict(newlist)
+
+    for key in newPlayer:
+            newPlayer[key] = player[key]
+
+    return newPlayer
 
 
 def writeToCSV(filename, object, writeOrAppend):
@@ -66,37 +116,8 @@ def writeToCSV(filename, object, writeOrAppend):
         wr = csv.writer(fp, dialect="excel")
         wr.writerow(object)
 
-
-def getFPLData():
-    r = requests.get(url)
-    data = json.loads(r.text)
-    all_players = data['elements']
-
-    player_dict = {}
-    for i in all_players:
-        player_dict[i['id']] = i['web_name']
-
-    # print(player_dict)
-
-    scorers = []
-    possible_scorers = {}
-    count = 0
-    n = 1
-
-    csvfile = "data.csv"
-    with open(csvfile, "w", newline='') as fp:
-        wr = csv.writer(fp, dialect="excel")
-        print(all_players[0])
-        wr.writerow(all_players[0])
-        for dict in all_players:
-            rowList = []
-            for k,v in dict.items():
-                print(v)
-                rowList.append(v)
-            wr.writerow(rowList)
-
-getFPLData()
-CleanDataCSV()
+players = getFPLData()
+CleanDataCSV(players)
 
 '''
 1. Remove injured players <50% chance of playing
@@ -105,18 +126,6 @@ CleanDataCSV()
 '''
 2. Remove extra columns from CSVs
 '''
-requiredColumns = ['goals_scored', 'assists', 'saves', 'clean_sheets', 'goals_conceded',
-                   'ea_index', 'transfers_in', 'transfers_out']
-
-
-commonColumns = ['id', 'web_name', 'team_code', 'element_type', 'status', 'now_cost', 'chance_of_playing_next_round',
-                 'value_season', 'minutes', 'bps', 'ict_index', 'form', 'threat', 'transfers_in_event',
-                 'transfers_out_event','total_points', 'ep_next', 'event_points']
-goalkeeperColumns = ['clean_sheets', 'goals_conceded']
-defenderColumns = ['clean_sheets', 'goals_conceded']
-midfielderColumns = ['goals_scored', 'assists', 'clean_sheets', 'goals_conceded']
-forwardColumns = ['goals_scored', 'assists']
-
 '''
 3. 
 '''
