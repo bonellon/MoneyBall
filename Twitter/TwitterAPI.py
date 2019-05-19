718c6bd70d32163a50ca25ceadd3195c3be1c2b7
Nicky <bonellon@tcd.ie>
2019-05-19 20:18:19 +0100

    updated twitter api request query + added first 5GWs. TODO: Add predictions before making more twitter requests.

--- Twitter/TwitterAPI.py
+++ Twitter/TwitterAPI.py
@@ -2,11 +2,19 @@ import requests
 import json
 
 endpoint = "https://api.twitter.com/1.1/tweets/search/fullarchive/main.json"
+bearer = "AAAAAAAAAAAAAAAAAAAAAOj2%2BQAAAAAAQUuNtoCEgrb9lAEfoZHP9EpGsYc%3DaXiehoNql3kYAJ5j1Bn3rojl2CTwpJB9uhfCbbxZ8l2gQzUJpR"
 
-headers = {"Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAAOj2%2BQAAAAAAQUuNtoCEgrb9lAEfoZHP9EpGsYc%3DaXiehoNql3kYAJ5j1Bn3rojl2CTwpJB9uhfCbbxZ8l2gQzUJpR", "Content-Type": "application/json"}
+currentGW = 4
+gwString = "gw"+str(currentGW)
 
-data = '{"query":"#fpl", "fromDate": "201802020000", "toDate": "201802240000"}'
+midnight = "0000"
 
-response = requests.post(endpoint,data=data,headers=headers).json()
 
-print(json.dumps(response, indent = 2))
+headers = {"Authorization":"Bearer "+bearer, "Content-Type": "application/json"}
+data = '{"query":"in out #fpl #'+gwString+'", "fromDate": "20180827'+midnight+'", "toDate": "20180901'+midnight+'"}'
+
+response = requests.post(endpoint, data=data, headers=headers).json()
+
+with open('GW_Tweets/'+gwString+'.json', 'w') as outFile:
+    json.dump(response, outFile)
+
