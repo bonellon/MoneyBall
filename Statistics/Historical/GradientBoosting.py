f9f67d450980ad430410eeb2db310ed01d0d3559
Nicky <bonellon@tcd.ie>
2019-06-04 14:45:43 +0100

    updated gitignore

--- Statistics/Historical/GradientBoosting.py
+++ Statistics/Historical/GradientBoosting.py
@@ -1,6 +1,6 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
