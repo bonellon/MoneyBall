5ae6c9524fbb32c7dfea876543543d8f6874fa9f
Nicky <bonellon@tcd.ie>
2019-06-11 20:40:08 +0100

    switched to google searching queries and analyzing all links

--- Blogs/blogs.py
+++ Blogs/blogs.py
@@ -2,12 +2,55 @@ import pandas as pd
 import Blogs.combinator as cb
 import Blogs.AylienAPI as aylien
 
+from googlesearch import search
+
 currentGW = 38
 
-def iterateBlogs():
-    blogs = pd.read_csv("blogs.csv", delimiter=',', sep='\n')
-    players = pd.read_csv("players.csv", delimiter=',', sep='\n')
+def googleSearch():
 
+    query = 'Fantasy Premier League gameweek "'+str(currentGW)+'"'
+    urls = []
+    print(query)
+    for url in search(query, stop=500, pause=1.0, tbs="qdr:y"):
+        print("Appending: "+str(len(urls)))
+        urls.append(url)
+
+    query = 'FPL gameweek '+str(currentGW)
+
+    print(query)
+    for url in search(query, stop=300, pause=1.0, tbs="qdr:y"):
+        if url not in urls:
+            print("Appending: "+str(len(urls)))
+            urls.append(url)
+
+    query = 'FPL budget gameweek ' + str(currentGW)
+
+
+    print(query)
+    for url in search(query, stop=50, pause=1.0, tbs="qdr:y"):
+        if url not in urls:
+            print("Appending: " + str(len(urls)))
+            urls.append(url)
+
+    query = 'FPL injury gameweek ' + str(currentGW)
+    for url in search(query, stop=50, pause=1.0, tbs="qdr:y"):
+        if url not in urls:
+            print("Appending: " + str(len(urls)))
+            urls.append(url)
+
+    with open('urls.txt', 'w') as f:
+        for item in urls:
+            f.write("%s\n" % item)
+
+    for i in range(0, len(urls)):
+        getRatings(urls[i], players, str(i))
+        players.to_csv("playersResult.csv")
+
+    getTotalRatings(players)
+    print(players.head())
+    players.to_csv("playersResult.csv")
+
+def iterateBlogs():
     for index, row in blogs.iterrows():
         url = row["URL"]
         change = row["Change"]
@@ -22,6 +65,7 @@ def iterateBlogs():
 
 
 def getRatings(url, players, blogCount):
+    print("Analysing "+str(blogCount)+"\n")
     json = aylien.getRatings(url)
 
     players[blogCount] = pd.Series(0.00, index=players.index)
@@ -30,7 +74,6 @@ def getRatings(url, players, blogCount):
         name = entity['mentions'][0]["text"]
         sentiment = entity['overall_sentiment']
 
-        print(name+" : "+sentiment["polarity"]+". Confidence: "+str(sentiment["confidence"]))
         matchName(name, sentiment, players, str(blogCount))
 
 def getTotalRatings(players):
@@ -47,11 +90,10 @@ def getTotalRatings(players):
 def matchName(name, sentiment, players, blogCounter):
     for index, row in players.iterrows():
         if(name in row["fullName"] or row["secondName"] in name or name in row['cleanName']):
-            print("MATCHED! : "+name + "  -  "+row['fullName'])
+            print("\tMATCHED! : "+name + "  -  "+row['fullName'])
             sentimentScore = getSentiment(sentiment)
             players.set_value(index, blogCounter, sentimentScore)
             return
-    print("No match found for : " + name)
 
 def getSentiment(sentiment):
     confidence = sentiment["confidence"]
@@ -63,4 +105,9 @@ def getSentiment(sentiment):
         return confidence
     return confidence-1.00
 
-iterateBlogs()
+
+blogs = pd.read_csv("blogs.csv", delimiter=',', sep='\n')
+players = pd.read_csv("players.csv", delimiter=',', sep='\n')
+
+#iterateBlogs()
+googleSearch()
