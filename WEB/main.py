d8067ca7cfb9fb989273e401e50231f399de5859
LAPTOP-BIOU57LM\Nicky <bonellon@tcd.ie>
2019-06-27 14:58:22 +0100

    connected to python backend

--- WEB/main.py
+++ WEB/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 import json
 
-
 app = Flask(__name__)
 
 
@@ -10,6 +9,20 @@ def welcome():
     app.logger.info("Successfully invoked the index route")
     return "Welcome to Adaptive Itinerarizer"
 
+
+@app.route('/summary')
+def summary():
+    response = app.response_class(
+        response= json.dumps({
+            "username":"admin",
+            "email":"admin@aol.com",
+            "id":"admin1"}
+        ),
+        status=200,
+        mimetype='application/json'
+    )
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/suggest', methods=['POST'])
 def suggest_places():
@@ -20,7 +33,7 @@ def suggest_places():
     app.logger.info(places)
     return jsonify(user_id=user_id, user_name=user_name, places=places)
     '''
-    return "OII"
+    return "No upcoming gameweek found. Check back later!"
     # return str(input_data)
 
 # If we're running in stand alone mode, run the application
